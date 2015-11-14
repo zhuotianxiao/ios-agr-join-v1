36a6d1e785b28fdd3a0332dcd6e4fe035c713ae9
findview <findview@findviewtekimac-mini.local>
2015-11-14 08:56:44 +0800

    Initial Commit

---
+++ agr-join-v1/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  agr-join-v1
+//
+//  Created by findview on 15/11/14.
+//  Copyright © 2015年 cn.findview. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ agr-join-v1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  agr-join-v1
+//
+//  Created by findview on 15/11/14.
+//  Copyright © 2015年 cn.findview. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
