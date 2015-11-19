63ef837a97ae8a868cd76a51d39f586cc506f24f
findview <findview@findviewtekimac-mini.local>
2015-11-19 21:27:47 +0800

    移动图片

--- agr-join-v1/ViewController.m
+++ agr-join-v1/ViewController.m
@@ -9,6 +9,8 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *previewIcon;
+
 @end
 
 @implementation ViewController
@@ -18,7 +20,43 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
+                                                                   
+}
+- (IBAction)click:(UIButton*)sender {
     
+    [UIView beginAnimations:nil context:nil];
+    [UIView setAnimationDuration:2];
+    CGAffineTransform transForm  = self.previewIcon.transform;
+    switch (sender.tag) {
+        case 0:
+            
+            //向上移动
+            transForm = CGAffineTransformTranslate(transForm, 0, -20);//向上移动20
+            break;
+        case 1:
+            
+            //向右移动
+            transForm = CGAffineTransformTranslate(transForm, 20, 0);//向上移动20
+            break;
+            
+        case 2:
+            
+            //向下移动
+            transForm = CGAffineTransformTranslate(transForm, 0, 20);//向上移动20
+            break;
+            
+        case 3:
+            
+            //向左移动
+            transForm = CGAffineTransformTranslate(transForm, -20, 0);//向上移动20
+            break;
+            
+        default:
+            break;
+    }
+    
+    self.previewIcon.transform = transForm;
+    [UIView commitAnimations];
 }
 
 @end
